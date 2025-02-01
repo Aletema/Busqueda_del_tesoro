a088d9b92ba2e9e7ec964b92fe94ff6e6004fc20
Alexis Trejo <alexisemanueltrejo@gmail.com>
2025-02-01 12:54:49 -0300

    Mejoramos e juego para que sea responsivo y mas atractivo

--- juego.js
+++ juego.js
@@ -1,26 +1,36 @@
-// coordenadas del tesoro
+// Dimensiones del mapa
 const WIDTH = 500;
-const HEIGH = 500;
+const HEIGHT = 500;
 
+// Generamos coordenadas aleatorias del tesoro
 let objetivo = {
   x: numeroAleatorio(WIDTH),
-  y: numeroAleatorio(HEIGH)
+  y: numeroAleatorio(HEIGHT)
 };
 
-// click handler
+// Referencias a los elementos HTML
 let $mapa = document.querySelector('#mapa');
 let $distance = document.querySelector('#distancia');
 let clicks = 0;
 
+// Evento cuando el jugador hace clic en el mapa
 $mapa.addEventListener('click', function (evento) {
-  console.log('click');
   clicks++;
   let distance = distancia(evento, objetivo);
   let distanceHint = getDistanciaPista(distance);
   $distance.innerHTML = `<h1>${distanceHint}</h1>`;
 
+  // Si está muy cerca del tesoro
   if (distance < 20) {
-    alert(`GENIAL, ENCONTRASTE EL TESORO EN: ${clicks} clicks!`);
-    location.reload();
+    Swal.fire({
+      title: "¡GENIAL, ENCONTRASTE EL TESORO! 🏆",
+      text: `Lo lograste en ${clicks} clicks.`,
+      icon: "success",
+      confirmButtonText: "Jugar de nuevo",
+      allowOutsideClick: false,
+      allowEscapeKey: false
+    }).then(() => {
+      location.reload(); // Recarga la página para jugar de nuevo
+    });
   }
 });
